f35ff867eb38fb030d0cdd2e75bb93fae08c725e
F-FFF-F <ffffffffg@skiff.com>
2023-12-12 18:55:55 +0300

    feat: create user persistent directories at boot

--- hosts/common/users/f/default.nix
+++ hosts/common/users/f/default.nix
@@ -50,4 +50,27 @@ in {
     useGlobalPkgs = true;
     useUserPackages = true;
   };
+
+  systemd.tmpfiles = {
+    rules = [
+      "d /persisthome/f/ 0750 f users -"
+      "d /persist/home/f/Downloads 0750 f users -"
+      "d /persist/home/f/Music 0750 f users -"
+      "d /persist/home/f/Pictures 0750 f users -"
+      "d /persist/home/f/Documents 0750 f users -"
+      "d /persist/home/f/Videos 0750 f users -"
+      "d \"/persist/home/f/VirtualBox VMs\" 0750 f users -"
+      "d /persist/home/f/VM 0750 f users -"
+      "d /persist/home/f/Templates 0750 f users -"
+      "d /persist/home/f/Public 0750 f users -"
+      "d /persist/home/f/Desktop 0750 f users -"
+      "d /persist/home/f/.gnupg 0750 f users -"
+      "d /persist/home/f/.ssh 0750 f users -"
+      "d /persist/home/f/.nixops 0750 f users -"
+      "d /persist/home/f/.local/share/keyrings 0750 f users -"
+      "d /persist/home/f/.local/share/direnv 0750 f users -"
+      "d /persist/home/f/.local/bin 0750 f users -"
+      "d /persist/home/f/NixConfig 0750 f users -"
+    ];
+  };
 }
