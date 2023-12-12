e086315e257d1661fbc50ab88218f614d0efefe2
F-FFF-F <ffffffffg@skiff.com>
2023-12-12 19:04:45 +0300

    fix: create more necessary persistent directories

--- hosts/common/users/f/default.nix
+++ hosts/common/users/f/default.nix
@@ -53,7 +53,7 @@ in {
 
   systemd.tmpfiles = {
     rules = [
-      "d /persisthome/f/ 0750 f users -"
+      "d /persist/home/f/ 0750 f users -"
       "d /persist/home/f/Downloads 0750 f users -"
       "d /persist/home/f/Music 0750 f users -"
       "d /persist/home/f/Pictures 0750 f users -"
@@ -67,6 +67,7 @@ in {
       "d /persist/home/f/.gnupg 0750 f users -"
       "d /persist/home/f/.ssh 0750 f users -"
       "d /persist/home/f/.nixops 0750 f users -"
+      "d /persist/home/f/.local 0750 f users -"
       "d /persist/home/f/.local/share/keyrings 0750 f users -"
       "d /persist/home/f/.local/share/direnv 0750 f users -"
       "d /persist/home/f/.local/bin 0750 f users -"
