e75791ae777bcb038bb6923ad1de04ec2335c28d
F-FFF-F <ffffffffg@skiff.com>
2023-12-13 19:09:46 +0300

    feat: add image optimization packages

---
+++ homes/f/features/image_optimization/cli/default.nix
@@ -0,0 +1,37 @@
+{ pkgs, config, ... }: {
+  home.packages = with pkgs; [
+    image_optim
+    # uses
+    advancecomp
+    gifsicle
+    jhead
+    jpegoptim
+    jpeg-archive
+    libjpeg
+    optipng
+    oxipng
+    pngcrush
+    pngquant
+    nodePackages_latest.svgo
+
+    imageworsener
+    pngloss
+    flaca
+    trimage
+
+    mozjpeg
+    libjpeg_original
+    libjxl
+    imlib2Full
+    libimagequant
+
+    imagemagickBig
+    graphicsmagick
+    epeg
+    freeimage
+    jhead
+    mjpegtoolsFull
+    vips
+    qoi
+  ];
+}
