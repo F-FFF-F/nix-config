13adfc2a5025318690343b97ea56b5a45cde20cd
F-FFF-F <ffffffffg@skiff.com>
2023-12-13 15:27:39 +0300

    feat: add plugins to network manager

--- hosts/common/optional/network-manager.nix
+++ hosts/common/optional/network-manager.nix
@@ -1,6 +1,13 @@
-{
+{ pkgs, ... }: {
   networking = {
     useDHCP = false;
-    networkmanager = { enable = true; };
+    networkmanager = {
+      enable = true;
+      plugins = with pkgs; [
+        networkmanager-openvpn
+        networkmanager-l2tp
+        networkmanager_strongswan
+      ];
+    };
   };
 }
