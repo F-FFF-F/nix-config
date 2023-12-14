3720f0dd76764ab5945954236c10cfbbd27c9875
F-FFF-F <ffffffffg@skiff.com>
2023-12-14 18:47:22 +0300

    feat: add more packages

--- homes/f/features/hardware_monitor/cli/default.nix
+++ homes/f/features/hardware_monitor/cli/default.nix
@@ -1,11 +1,13 @@
 { pkgs, config, ... }: {
-  home.packages = with pkgs;
-    [
-      amdgpu_top
-      vdpauinfo
-      libva-utils
-      vulkan-tools
-      radeontop
-      glxinfo
-    ];
+  home.packages = with pkgs; [
+    amdgpu_top
+    vdpauinfo
+    libva-utils
+    vulkan-tools
+    radeontop
+    glxinfo
+    lsof
+    lshw
+    hw-probe
+  ];
 }
