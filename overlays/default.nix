c92ceee2eea90550b0a7d7d15ab2b59b7ad8ee15
F-FFF-F <ffffffffg@skiff.com>
2023-12-17 12:45:56 +0300

    feat: update overlays

--- overlays/default.nix
+++ overlays/default.nix
@@ -1,5 +1,16 @@
 # This file defines overlays
-{inputs, ...}: {
-  additions = final: _prev: import ../pkgs {pkgs = final;};
-  modifications = final: prev: {};
+{ inputs, ... }: {
+  # For every flake input, aliases 'pkgs.inputs.${flake}' to
+  # 'inputs.${flake}.packages.${pkgs.system}' or
+  # 'inputs.${flake}.legacyPackages.${pkgs.system}'
+  flake-inputs = final: _: {
+    inputs = builtins.mapAttrs (_: flake:
+      let
+        legacyPackages = ((flake.legacyPackages or { }).${final.system} or { });
+        packages = ((flake.packages or { }).${final.system} or { });
+      in if legacyPackages != { } then legacyPackages else packages) inputs;
+  };
+
+  additions = final: _prev: import ../pkgs { pkgs = final; };
+  modifications = final: prev: { };
 }
