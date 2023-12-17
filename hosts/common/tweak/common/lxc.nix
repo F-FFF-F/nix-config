d8996d18ae8e33097c81f97d5034d2eedba8e4b3
F-FFF-F <ffffffffg@skiff.com>
2023-12-17 14:45:17 +0300

    feat: update tweak

--- hosts/common/tweak/common/lxc.nix
+++ hosts/common/tweak/common/lxc.nix
@@ -4,6 +4,8 @@
     kernel = {
       sysctl = {
         "kernel.keys.maxkeys" = 2000;
+        "kernel.keys.maxbytes" = 2000000;
+        "net.core.bpf_jit_limit" = 1000000000;
         "kernel.dmesg_restrict" = 1;
         "net.ipv4.neigh.default.gc_thresh3" = 8192;
         "net.ipv6.neigh.default.gc_thresh3" = 8192;
