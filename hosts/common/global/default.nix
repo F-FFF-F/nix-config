127496949427d92374c573c1068b811d3d791858
F-FFF-F <ffffffffg@skiff.com>
2023-12-17 21:13:08 +0300

    feat: update nix settings

--- hosts/common/global/default.nix
+++ hosts/common/global/default.nix
@@ -1,4 +1,4 @@
-{ inputs, outputs, ... }: {
+{ inputs, outputs, lib, pkgs, ... }: {
   imports = [
     inputs.home-manager.nixosModules.home-manager
     inputs.chaotic.nixosModules.default
@@ -19,6 +19,21 @@
       oraclejdk.accept_license = true;
 
       permittedInsecurePackages = [ "electron-25.9.0" ];
+    };
+  };
+
+  nix = {
+    package = lib.mkDefault pkgs.nix;
+    settings = {
+      experimental-features = [ "nix-command" "flakes" "repl-flake" ];
+      trusted-users = [ "root" "@admin" "@wheel" "f" ];
+      extra-substituters =
+        [ "https://nyx.chaotic.cx/" "https://nix-community.cachix.org" ];
+      extra-trusted-public-keys = [
+        "nyx.chaotic.cx-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+        "chaotic-nyx.cachix.org-1:HfnXSw4pj95iI/n17rIDy40agHj12WfF+Gqk6SonIT8="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      ];
     };
   };
 }
