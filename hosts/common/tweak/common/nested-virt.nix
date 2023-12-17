cd5fc4f9f6bc90fa6d4d920365a9edc8dacc33d9
F-FFF-F <ffffffffg@skiff.com>
2023-12-17 14:42:06 +0300

    feat: update tweak

--- hosts/common/tweak/common/nested-virt.nix
+++ hosts/common/tweak/common/nested-virt.nix
@@ -3,6 +3,8 @@
     extraModprobeConfig = ''
       options kvm-amd nested=1
       options kvm-intel nested=1
+      options kvm_intel emulate_invalid_guest_state=0
+      options kvm ignore_msrs=1
     '';
   };
 }
